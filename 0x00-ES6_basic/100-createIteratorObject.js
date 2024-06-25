0ed09fbaa4eb4fd25286ec21ced1aaca7aca6836
brikhotso <brenda.rikhotso@gmail.com>
2024-06-25 02:06:04 +0200

    iterate over an array

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,19 @@
 export default function createIteratorObject(report) {
+  const employees = Object.values(report.allEmployees).flat();
+  let index = 0;
 
+  return {
+    [Symbol.iterator]() {
+      return this;
+    },
+
+    next() {
+      if (index < employees.length) {
+        const value = employees[index];
+        index += 1;
+        return { done: false, value };
+      }
+      return { done: true };
+    },
+  };
 }

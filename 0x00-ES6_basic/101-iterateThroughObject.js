2d2fe81da1025d588ae404524cc15bc8646dac99
brikhotso <brenda.rikhotso@gmail.com>
2024-06-25 02:38:09 +0200

    add separator and append employee

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,10 @@
 export default function iterateThroughObject(reportWithIterator) {
-    return reportWithIterator.join(' | ');
+  let result = '';
+  for (const employee of reportWithIterator) {
+    if (result) {
+      result += ' | '; // Add separator if result is not empty
+    }
+    result += employee; // Append the current employee name
+  }
+  return result;
 }

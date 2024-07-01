2cc982aec7a494b0b3691faa2f6df6225636ee33
brikhotso <brenda.rikhotso@gmail.com>
2024-07-01 22:59:31 +0200

    reduce ids

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function getStudentIdsSum(objects) {
-    return objects.reduce((acc, student) => acc + student.id, 0);
+  return objects.reduce((acc, student) => acc + student.id, 0);
 }

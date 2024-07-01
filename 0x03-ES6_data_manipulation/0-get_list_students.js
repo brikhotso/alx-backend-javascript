85f9ddd8fbd49da1b09fbb39350f5a4d505e09d5
brikhotso <brenda.rikhotso@gmail.com>
2024-07-01 22:58:16 +0200

    reduce ids

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,22 +1,7 @@
-function validateStudent({ id, firstName, location }) {
-  if (typeof id !== 'number') {
-    throw new TypeError('ID must be a number');
-  }
-  if (typeof firstName !== 'string') {
-    throw new TypeError('FirstName must be a string');
-  }
-  if (typeof location !== 'string') {
-    throw new TypeError('Location must be a string');
-  }
-}
-
 export default function getListStudents() {
   const student1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
   const student2 = { id: 2, firstName: 'James', location: 'Columbia' };
   const student3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
-
-  // Validate each student before returning the list
-  [student1, student2, student3].forEach(validateStudent);
 
   return [student1, student2, student3];
 }

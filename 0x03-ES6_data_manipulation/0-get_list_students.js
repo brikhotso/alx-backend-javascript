8845c526af17e4615a2e56e3294903c82a1f26b5
brikhotso <brenda.rikhotso@gmail.com>
2024-07-02 02:20:54 +0200

    return list of students

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
-  const student1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
-  const student2 = { id: 2, firstName: 'James', location: 'Columbia' };
-  const student3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
-
-  return [student1, student2, student3];
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }

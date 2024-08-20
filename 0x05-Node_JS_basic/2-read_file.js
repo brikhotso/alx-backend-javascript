060614944bd02b4dbc7ce5f78cc2f1fb91f5abce
brikhotso <brenda.rikhotso@gmail.com>
2024-08-20 15:55:54 +0200

    fix warnings

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -28,7 +28,6 @@ function countStudents(path) {
     for (const [field, names] of Object.entries(fieldCount)) {
       console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
     }
-
   } catch (err) {
     throw new Error('Cannot load the database');
   }
